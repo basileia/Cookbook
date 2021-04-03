94db6040014e25878af1901328fc0d403cad6371
basileia <jane.novak@centrum.cz>
2021-04-03 20:51:07 +0200

    Add project files.

---
+++ Ingredient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookbook
+{
+    class Ingredient
+    {
+        public string Name { get; private set; }
+        public int Quantity { get; private set; }
+        public string Unit { get; private set; }
+
+        public Ingredient(string name, int quantity, string unit)
+        {
+            Name = name;
+            Quantity = quantity <= 0 ? 0 : quantity;
+            Unit = unit;
+        }
+
+    }
+}
