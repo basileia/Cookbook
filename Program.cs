59a358d7d08a04e9b4cf97cb67c25d6423d7b124
basileia <jane.novak@centrum.cz>
2021-05-04 12:17:18 +0200

    fix: remove hardcoded path from Cookbook

--- Program.cs
+++ Program.cs
@@ -1,20 +1,28 @@
-﻿namespace Cookbook
+﻿using System;
+using System.IO;
+
+namespace Cookbook
 {
     class Program
     {
          static void Main()
         {
+            string sourceDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Cookbook");
+            string sourceFile = Path.Combine(sourceDirectory, "recipes.json");
+            
+            if (!Directory.Exists(sourceDirectory))
+            {
+                Directory.CreateDirectory(sourceDirectory);
+            }
 
-            Cookbook cookbook = new Cookbook();
-
-            cookbook.LoadRecipesFromJson();
+            Cookbook cookbook = Cookbook.LoadRecipesFromJson(sourceFile);
 
             while (cookbook.ShowMenu)
             {
                 cookbook.ShowMenu = Menu.MainMenu(cookbook);
             }
             
-            cookbook.PutRecipesToJson();
+            Cookbook.PutRecipesToJson(cookbook, sourceFile);
              
         }
 
